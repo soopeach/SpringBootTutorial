46c42be8cce18a864a0fb2bb6c8c97eef7fb9c83
soopeach <hsjeon584@gmail.com>
2023-05-26 13:39:05 +0900

    feat: Date 관리를 해줄 BaseTimeEntity 구현 및 Posts에 적용

--- src/main/java/org/soopeach/springboot/domain/posts/Posts.java
+++ src/main/java/org/soopeach/springboot/domain/posts/Posts.java
@@ -3,13 +3,14 @@ package org.soopeach.springboot.domain.posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.soopeach.springboot.domain.BaseTimeEntity;
 
 import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
