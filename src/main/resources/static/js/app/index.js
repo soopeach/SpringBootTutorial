c3a8d5fdef9d5260a5ecf8350fb5b1c9de105309
soopeach <hsjeon584@gmail.com>
2023-05-27 13:16:55 +0900

    feat: 게시글 삭제 기능 구현

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -3,8 +3,8 @@ const main = {
 
         const _this = this;
         $('#btn-save').on('click', () => _this.save())
-
         $('#btn-update').on('click', () => _this.update())
+        $('#btn-delete').on('click', () => _this.delete())
 
     },
     save: function () {
@@ -47,8 +47,21 @@ const main = {
         }).fail((error) => {
             alert(JSON.stringify(error))
         })
+    },
+    delete: () => {
+        const id = $('#id').val()
 
-    }
-}
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+        }).done(() => {
+            alert('글이 삭제되었습니다.')
+            window.location.href = '/'
+        }).fail((error) => {
+            alert(JSON.stringify(error))
+        })
+    }}
 
 main.init()
